aa7ea3d8d9ddff10844575389424f89c4031c5a3
arcazj <arcazj@gmail.com>
2025-11-23 17:32:00 -0500

    update gantt timeline
    Signed-off-by: arcazj <arcazj@gmail.com>

--- js/ganttTimelineLoader.js
+++ js/ganttTimelineLoader.js
@@ -17,8 +17,6 @@ const MS_DAY = 24 * 60 * 60 * 1000 *2;
 const MS_YEAR = 365.25 * MS_DAY;
 const OVERVIEW_SPACING_TARGET = 360/2;
 const OVERVIEW_MIN_RANGE = MS_YEAR * 1.5;
-const DEFAULT_DETAIL_RANGE = MS_DAY * 180; // roughly six months
-const OVERVIEW_PADDING = MS_YEAR * 0.5; // half-year padding on each side
 
 function createHudElements() {
     const container = document.createElement('div');
@@ -173,38 +171,11 @@ export function initTimeline(satellites, arg2, arg3) {
         scheduleDraw();
     });
 
-    const earliestLaunch = timelineData[0].time;
-    const latestLaunch = timelineData[timelineData.length - 1].time;
-    const dataRange = latestLaunch - earliestLaunch;
-    const overviewPadding = Math.max(OVERVIEW_PADDING, dataRange * 0.05);
-    const overviewRange = Math.max(OVERVIEW_MIN_RANGE, dataRange + overviewPadding * 2);
+    let detailStart = centerDate.getTime() - MS_DAY * 3.5;
+    let detailEnd = centerDate.getTime() + MS_DAY * 3.5;
 
-    let overviewStart = earliestLaunch - overviewPadding;
-    let overviewEnd = overviewStart + overviewRange;
-
-    const centerTime = Math.min(Math.max(centerDate.getTime(), earliestLaunch), latestLaunch);
-    const detailRange = DEFAULT_DETAIL_RANGE;
-    let detailStart = centerTime - detailRange / 2;
-    let detailEnd = centerTime + detailRange / 2;
-
-    function clampDetailToOverview() {
-        const overviewSpan = overviewEnd - overviewStart;
-        let detailSpan = detailEnd - detailStart;
-        if (detailSpan > overviewSpan) {
-            detailSpan = overviewSpan;
-        }
-
-        if (detailStart < overviewStart) {
-            detailStart = overviewStart;
-            detailEnd = detailStart + detailSpan;
-        }
-        if (detailEnd > overviewEnd) {
-            detailEnd = overviewEnd;
-            detailStart = detailEnd - detailSpan;
-        }
-    }
-
-    clampDetailToOverview();
+    let overviewStart = new Date(centerDate.getUTCFullYear() - 10, 0, 1).getTime();
+    let overviewEnd = new Date(centerDate.getUTCFullYear() + 10, 0, 1).getTime();
 
     let detailPositions = [];
 
@@ -413,7 +384,6 @@ export function initTimeline(satellites, arg2, arg3) {
         const deltaMs = (dx / width) * detailDragRange;
         detailStart = detailDragRangeStart - deltaMs;
         detailEnd = detailStart + detailDragRange;
-        clampDetailToOverview();
         scheduleDraw();
     });
 
@@ -446,7 +416,6 @@ export function initTimeline(satellites, arg2, arg3) {
         const tRatio = (mouseTime - detailStart) / range;
         detailStart = mouseTime - newRange * tRatio;
         detailEnd = detailStart + newRange;
-        clampDetailToOverview();
         scheduleDraw();
     }, { passive: false });
 
@@ -488,7 +457,6 @@ export function initTimeline(satellites, arg2, arg3) {
             const duration = detailEnd - detailStart;
             detailStart = newStart;
             detailEnd = detailStart + duration;
-            clampDetailToOverview();
             scheduleDraw();
             return;
         }
@@ -501,7 +469,6 @@ export function initTimeline(satellites, arg2, arg3) {
             const detailRange = detailDuringOverviewEnd - detailDuringOverviewStart;
             detailStart = detailDuringOverviewStart - deltaMs;
             detailEnd = detailStart + detailRange;
-            clampDetailToOverview();
             scheduleDraw();
         }
     });
@@ -527,7 +494,6 @@ export function initTimeline(satellites, arg2, arg3) {
         const tRatio = (mouseTime - overviewStart) / range;
         overviewStart = mouseTime - newRange * tRatio;
         overviewEnd = overviewStart + newRange;
-        clampDetailToOverview();
         scheduleDraw();
     }, { passive: false });
 
